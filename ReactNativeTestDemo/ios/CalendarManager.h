e46de5e27fc415bd067cb81580f94b993c5d6518
lyon <lyon_lzy@sina.cn>
2016-07-29 19:00:08 +0800

    修改

---
+++ ReactNativeTestDemo/ios/CalendarManager.h
@@ -0,0 +1,13 @@
+//
+//  CalendarManager.h
+//  ReactNativeTestDemo
+//
+//  Created by 众网合一 on 16/7/29.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalendarManager : NSObject
+
+@end
---
+++ ReactNativeTestDemo/ios/CalendarManager.m
@@ -0,0 +1,13 @@
+//
+//  CalendarManager.m
+//  ReactNativeTestDemo
+//
+//  Created by 众网合一 on 16/7/29.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import "CalendarManager.h"
+
+@implementation CalendarManager
+
+@end
